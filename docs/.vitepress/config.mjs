4a528eb1e10ec6a689ac9929dd6cec8b2e848d83
lixinzhang-cn <86455292@qq.com>
2024-06-11 17:37:06 +0800

    修改配置文件

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: '首页', link: '/' },
+      { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
